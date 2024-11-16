a8fccf2d21e076c1ce631f2a45bb597e59c79067
johnRojas08 <johnrojas0808@gmail.com>
2024-11-16 17:54:15 +0100

    Actualizando nombre del fichero, apellidoNombre

--- src/RojasJohnEjercicio1.java
+++ src/RojasJohnEjercicio1.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class JohnRojasEjercicio1 {
+public class RojasJohnEjercicio1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Random random = new Random();
